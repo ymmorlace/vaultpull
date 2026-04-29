59a4082d614a12402aaa1f255c46766a6f4246f4
Yuri Morlace <ymmorlace@users.noreply.github.com>
2026-04-30 01:55:40 +0800

    test: export sanitizeKey for white-box testing

--- internal/sync/export_test.go
+++ internal/sync/export_test.go
@@ -5,3 +5,9 @@ package sync
 func ExportToEnvKey(keyPath, field string) string {
 	return toEnvKey(keyPath, field)
 }
+
+// ExportSanitizeKey exposes the unexported sanitizeKey function for white-box
+// testing from the sync_test package.
+func ExportSanitizeKey(s string) string {
+	return sanitizeKey(s)
+}
